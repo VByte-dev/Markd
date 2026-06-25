17a4830e55ed4a91117898617db121e5eef277c5
Vedhesh <vbyte.02@gmail.com>
2026-06-25 18:42:39 +0530

    Enhance typography in StateMessage component for improved readability

--- src/components/StateMessage.jsx
+++ src/components/StateMessage.jsx
@@ -4,8 +4,8 @@ let StateMessage = (props) => {
   return (
     <>
       <div className="bg-light p-4 rounded-xl border-2 border-mid">
-        <p className="font-[coolvetica] text-deep text-center">{title}</p>
-        <p className="font-[coolvetica] text-deep text-center opacity-70 mt-1">
+        <p className="font-[coolvetica] text-deep text-center text-base md:text-lg">{title}</p>
+        <p className="font-[coolvetica] text-deep text-center opacity-70 mt-1 text-sm md:text-base">
           {description}
         </p>
       </div>
