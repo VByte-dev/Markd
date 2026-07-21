e51eee24c6cb136ef8ebe326b2eb9188906d6548
Vedhesh <vbyte.02@gmail.com>
2026-07-21 21:03:36 +0530

    readme

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res)=>{
+  res.send("hello visitor!");
+})
+
 app.listen(3000, () => {
   console.log("Server started...");
 });
