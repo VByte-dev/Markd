e4247fd4380e0519cdb366b4f2ee516735a0ad8f
Vedhesh <vbyte.02@gmail.com>
2026-08-18 16:33:00 +0530

    Log out feature

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,11 @@
 // Components
 import { useState } from "react";
 import AddBookmark from "./AddBookmark";
+import {useNavigate} from "react-router-dom";
 
 let Header = () => {
+  const navigateTo = useNavigate();
+
   let [isFormClose, setIsFormClose] = useState(true);
 
   let handleIsFormClose = (formState) => {
@@ -15,7 +18,7 @@ let Header = () => {
         <h1 className="text-deep font-[instrumentSerif] text-2xl md:text-3xl">
           markd.
         </h1>
-        <div>
+        <div className="flex gap-4">
           <button
             className="bg-deep text-surface rounded font-[bricolage] text-xs py-2 px-4 md:text-sm cursor-pointer"
             onClick={() => {
@@ -23,6 +26,15 @@ let Header = () => {
             }}
           >
             Add Bookmark
+          </button>
+          <button
+            className="bg-red-700 text-surface rounded font-[bricolage] text-xs py-2 px-4 md:text-sm cursor-pointer"
+            onClick={() => {
+              localStorage.removeItem("token");
+              navigateTo("/login");
+            }}
+          >
+            Log Out
           </button>
         </div>
       </section>
